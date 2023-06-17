8acb283c02ad4feb6a176abd445a80cde1965e2a
musiakam <kamil.musial@tietoevry.com>
2023-06-17 13:03:20 +0200

    1

--- 7_duzy_program.py
+++ 7_duzy_program.py
@@ -17,7 +17,7 @@ class MyGUI:
         self.label.pack(padx=10, pady=10)
 
         self.textbox = tk.Text(self.root, height=3, font=('Arial', 18))
-#        self.textbox.bind('<KeyPress>', self.short)
+        self.textbox.bind('<KeyPress>', self.wprowadzony_tekst)
         self.textbox.pack(padx=10, pady=10)
 
         self.check_state = tk.IntVar()
@@ -26,6 +26,11 @@ class MyGUI:
 
         self.button = tk.Button(self.root, text='nacisnij', font=('Arial', 16), command=self.przycisk1)
         self.button.pack(padx=10, pady=10)
+
+        self.button_clear = tk.Button(self.root, text='czysc', font=('Arial', 16), command=self.clear)
+        self.button_clear.pack(padx=10, pady=10)
+
+        self.root.protocol('WM_DELETE_WINDOW', self.zamknij)   #zamknięcie okna
 
         self.root.mainloop()
 
@@ -36,6 +41,21 @@ class MyGUI:
             print(self.textbox.get(   '1.0'  ,   tk.END   ))   #przepisz to,co wpisane
         else:  #wyświetl w dodatkowym oknie to, co napisane
             messagebox.showinfo(title='Wiadomosc', message=self.textbox.get(   '1.0'  ,   tk.END   ))
+
+    def wprowadzony_tekst(self, event):
+        # print(event)
+        # print(event.keysym)
+        # print(event.state)
+        if event.keysym == 'Return' and event.state == 12:
+            print('Nacisnieto ctrl+enter')
+            self.przycisk1()
+
+    def clear(self):
+        self.textbox.delete('1.0', tk.END)
+
+    def zamknij(self):
+        if messagebox.askyesno(title='Czy wyjsc?', message='Czy napewno chcesz zakonczyc program'):
+            self.root.destroy()
 
 MyGUI()
 
