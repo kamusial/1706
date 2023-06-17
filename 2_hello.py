f6c91fac9c37474d0f66d4ce32cfc4ec7051c8f6
musiakam <kamil.musial@tietoevry.com>
2023-06-17 09:49:05 +0200

    1

--- 2_hello.py
+++ 2_hello.py
@@ -4,10 +4,19 @@ root = tk.Tk()
 root.title('Tytul okna - pierwsze okna')
 root.geometry('400x300+500+200')
 root.iconbitmap('./icon.ico')
+#root.resizable(True, False)    $zablokowanie możliowści zmiany okna
+root.minsize(200, 200)
+root.maxsize(800, 600)
+root.attributes('-topmost', 1)   # zawsze na wierzchu
+root.attributes('-alpha', 0.9)   #przezroczystosc
+root.config(background='Light Blue')  #   #ccc albo #555
 
 message1 = tk.Label(root, text='Siema, pierwszy kod')
 message1.pack()
 message2 = tk.Label(root, text='Siema, drugi kod')
 message2.pack()
+
+exit_button = tk.Button(root, text='Wyjscie', command=lambda: root.quit())
+exit_button.pack(ipadx=55, ipady=10, expand=True)    #dodatkowe miejsce
 
 root.mainloop()
