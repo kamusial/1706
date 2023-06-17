8095d4d99faed9ff55916eea7880265453b576f7
musiakam <kamil.musial@tietoevry.com>
2023-06-17 11:58:26 +0200

    1

--- 7_duzy_program.py
+++ 7_duzy_program.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import messagebox
 
 class MyGUI:
     def __init__(self):
@@ -16,17 +17,25 @@ class MyGUI:
         self.label.pack(padx=10, pady=10)
 
         self.textbox = tk.Text(self.root, height=3, font=('Arial', 18))
+#        self.textbox.bind('<KeyPress>', self.short)
         self.textbox.pack(padx=10, pady=10)
 
         self.check_state = tk.IntVar()
         self.check = tk.Checkbutton(self.root, text='zaznacz', font=('Arial', 14), variable=self.check_state)
         self.check.pack(padx=10, pady=10)
 
-        self.button = tk.Button(self.root, text='nacisnij', font=('Arial', 16))
+        self.button = tk.Button(self.root, text='nacisnij', font=('Arial', 16), command=self.przycisk1)
         self.button.pack(padx=10, pady=10)
 
         self.root.mainloop()
 
+    def przycisk1(self):
+        # print('przycisk wcisniety')
+        # print(self.check_state.get())
+        if self.check_state.get() == 0:
+            print(self.textbox.get(   '1.0'  ,   tk.END   ))   #przepisz to,co wpisane
+        else:  #wyświetl w dodatkowym oknie to, co napisane
+            messagebox.showinfo(title='Wiadomosc', message=self.textbox.get(   '1.0'  ,   tk.END   ))
 
 MyGUI()
 
